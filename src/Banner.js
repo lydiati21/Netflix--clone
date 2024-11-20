10a44593d639f24a1d55416f9e83f0202b8e5dcc
lydiati21 <lydiatilahun12@gmail.com>
2024-11-21 00:39:04 +0300

    updated the comp

--- src/Banner.js
+++ src/Banner.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./Banner.css";
 import axios from "./axios";
 import requests from "./Requests";
